7b9e826cf3f6fc6f100682d894519703dd2edb19
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2025-03-06 22:31:23 +0900

    feat: 스타일 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741180396',resources: [
+    cacheName: 'chirpy-1741267826',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
