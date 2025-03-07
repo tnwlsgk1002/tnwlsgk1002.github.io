335e848baab65dc6c1ee6d0ad5ab489661014a5c
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2025-03-07 13:03:11 +0900

    feat: 프로필 배경 선 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741267826',resources: [
+    cacheName: 'chirpy-1741320104',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
