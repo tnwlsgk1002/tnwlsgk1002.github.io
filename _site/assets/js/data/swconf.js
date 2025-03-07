150a5db0f01a0506646af3fa4bb16d1354c6184d
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2025-03-07 13:19:00 +0900

    feat: sidebar 하단 버튼 스타일 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741320104',resources: [
+    cacheName: 'chirpy-1741321112',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
