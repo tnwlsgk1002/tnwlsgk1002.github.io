8dd76c4c48846fdba5de76e2a456a400a148115b
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2025-03-15 20:20:33 +0900

    fix: 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742036503',resources: [
+    cacheName: 'chirpy-1742037621',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
