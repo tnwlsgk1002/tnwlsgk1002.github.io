f9faa128a1ea491b0fe095d49fd98203e40450bd
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2025-03-15 19:49:01 +0900

    feat: 기본 포스팅 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742035599',resources: [
+    cacheName: 'chirpy-1742035703',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
